0f5101e069ad3167b09c93df8b7d5c084ea479d7
MisterZeus <oresteszoupanos@hotmail.com>
2015-02-21 17:59:38 +0000

    Redirect stderr to stdout
    With this setting, we redirect any remote shell errors to stdOUT.
    
    Code taken from
    http://jimmyg.org/blog/2009/working-with-python-subprocess.html#redirecting-stderr-to-stdout

--- rd_tool.py
+++ rd_tool.py
@@ -64,13 +64,13 @@ class Slot:
         if self.machine is None:
             print(GetTime(),'No support for local execution.')
             sys.exit(1)
-            self.p = subprocess.Popen(['metrics_gather.sh',work.filename], env=env, stdout=subprocess.PIPE)
+            self.p = subprocess.Popen(['metrics_gather.sh',work.filename], env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         else:
             self.p = subprocess.Popen(['ssh','-i','daala.pem','-o',' StrictHostKeyChecking=no',
                 'ec2-user@'+self.machine.host,
                 ('DAALA_ROOT=/home/ec2-user/daala/ x="'+str(work.quality)+'" CODEC="'+args.codec+
                     '" /home/ec2-user/rd_tool/metrics_gather.sh '+shellquote(input_path)
-                ).encode("utf-8")], env=env, stdout=subprocess.PIPE)
+                ).encode("utf-8")], env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     def busy(self):
         if self.p is None:
             return False
