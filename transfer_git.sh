358830efcc2d98bbf83a475d569f47056d5d03de
Thomas Daede <daede003@umn.edu>
2015-06-26 11:38:39 -0700

    More gcc debugging

--- transfer_git.sh
+++ transfer_git.sh
@@ -13,6 +13,7 @@ fi
 echo Building...
 pushd $DAALA_ROOT
 gcc -print-prog-name=cc1
+gcc -print-search-dirs
 ./autogen.sh; ./configure --enable-static --disable-shared --disable-player --disable-dump-images --enable-logging --enable-dump-recons ; make -j4
 popd
 
