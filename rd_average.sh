d87aab863bab21c344de2a4d3f356eb53340e684
Thomas Daede <daede003@umn.edu>
2016-06-15 03:31:00 -0700

    Add rd_average.sh from Daala repo

---
+++ rd_average.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then
+  echo "usage: OUTPUT=<label> $0 *.out"
+  exit 1
+fi
+
+TOTAL=total.out
+
+if [ -n "$OUTPUT" ]; then
+  TOTAL="$OUTPUT.out"
+fi
+
+if [ -e "$TOTAL" ]; then
+  echo "ERROR: $TOTAL already exists and will be included in average, please remove it first"
+  exit 1
+fi
+
+awk '{size[$1]+=$2;bytes[$1]+=$3;psnr[$1]+=$2*$4;psnrhvs[$1]+=$2*$5;ssim[$1]+=$2*$6;fastssim[$1]+=$2*$7;}END{for(i in size)print i,size[i],bytes[i],psnr[i]/size[i],psnrhvs[i]/size[i],ssim[i]/size[i],fastssim[i]/size[i];}' $@ | sort -n > $TOTAL
