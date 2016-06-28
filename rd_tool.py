e6c15a02dbb8186cb8df4f2ddb1a4b00d89dc9ff
Thomas Daede <daede003@umn.edu>
2016-06-28 11:33:22 -0700

    Only use one machine for image sets to reduce startup time.

--- rd_tool.py
+++ rd_tool.py
@@ -215,7 +215,10 @@ rd_print('0 out of',total_num_of_jobs,'finished.')
 #how many AWS instances do we want to spin up?
 #The assumption is each machine can deal with 18 threads,
 #so up to 18 jobs, use 1 machine, then up to 64 use 2, etc...
-num_instances_to_use = (31 + total_num_of_jobs) // 18
+if video_sets[args.set[0]]['type'] == 'image':
+    num_instances_to_use = 1
+else:
+    num_instances_to_use = (31 + total_num_of_jobs) // 18
 
 #...but lock AWS to a max number of instances
 max_num_instances_to_use = int(args.machines)
