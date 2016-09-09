dc030621fe2ef8390f96b1d46a202358f117c6c4
Thomas Daede <daede003@umn.edu>
2016-09-09 15:22:53 -0700

    Print to stderr.

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+import sys
 
 #our timestamping function, accurate to milliseconds
 #(remove [:-3] to display microseconds)
@@ -8,6 +9,6 @@ def get_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
 def rd_print(log, *args, **kwargs):
-    print(get_time(), *args, **kwargs)
+    print(get_time(), *args, file=sys.stderr, **kwargs)
     if log:
         print(get_time(), file=log, flush=True, *args, **kwargs)
