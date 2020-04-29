a95d34389fa2d7425d6aa5b604f1484f109ce169
Raphaël Zumer <rzumer@tebako.net>
2020-04-29 17:10:01 -0400

    Use installed decoders to test rav1e bitstream
    The decoders are now optional dependencies.
    In case no decoder is installed, bitstream
    testing will be skipped, and a message will
    be logged.

--- metrics_gather.sh
+++ metrics_gather.sh
@@ -93,10 +93,6 @@ if [ -z "$YUV2YUV4MPEG" ]; then
   export YUV2YUV4MPEG="$DAALATOOL_ROOT/tools/yuv2yuv4mpeg"
 fi
 
-if [ -z "$DAV1D" ]; then
-  export DAV1D="$DAALATOOL_ROOT/../dav1d/build/tools/dav1d"
-fi
-
 if [ -z "$CODEC" ]; then
   export CODEC=daala
 fi
@@ -227,14 +223,19 @@ thor-rt)
   ;;
 rav1e)
   $($TIMER $RAV1E $FILE --quantizer $x -o $BASENAME.ivf -r $BASENAME-rec.y4m --threads 1 $EXTRA_OPTIONS > $BASENAME-enc.out)
-  if [ -f "$DAV1D" ]; then
-    $($TIMERDEC $DAV1D -q -i $BASENAME.ivf -o $BASENAME.y4m)
+  if hash dav1d 2>/dev/null; then
+    $($TIMERDEC dav1d -q -i $BASENAME.ivf -o $BASENAME.y4m) || (echo "Corrupt bitstream detected!"; exit 98)
+  elif hash aomdec 2>dev/null; then
+    $($TIMERDEC aomdec --codec=av1 $AOMDEC_OPTS -o $BASENAME.y4m $BASENAME.ivf) || (echo "Corrupt bitstream detected!"; exit 98)
   else
-    $($TIMERDEC $AOMDEC --codec=av1 $AOMDEC_OPTS -o $BASENAME.y4m $BASENAME.ivf)
+    echo "AV1 decoder not found, desync/corruption detection disabled."
   fi
-  "$Y4M2YUV" "$BASENAME-rec.y4m" -o rec.yuv
-  "$Y4M2YUV" "$BASENAME.y4m" -o enc.yuv
-  cmp --silent rec.yuv enc.yuv || (echo "Reconstruction differs from output!"; exit 1)
+
+  if [ -f $BASENAME.y4m ]; then
+    "$Y4M2YUV" "$BASENAME-rec.y4m" -o rec.yuv
+    "$Y4M2YUV" "$BASENAME.y4m" -o enc.yuv
+    cmp --silent rec.yuv enc.yuv || (echo "Reconstruction differs from output!"; exit 98)
+  fi
   SIZE=$(stat -c %s $BASENAME.ivf)
   ;;
 svt-av1)
